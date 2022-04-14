fbb6a207c64670b17e9893861577a84bca12255c
pamela <abreupamm@gmail.com>
2022-04-14 17:33:55 -0300

    questao 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const specie = data.species.filter((animalSpecie) => animalSpecie.name === animal);
+  return specie.every((animal1) => animal1.residents.every((dadosAnimal) => dadosAnimal.age > age));
 }
 
 module.exports = getAnimalsOlderThan;
