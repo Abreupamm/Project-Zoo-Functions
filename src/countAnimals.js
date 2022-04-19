c2f3e4fb7900ced44b485c55b8af5b290ff0638c
pamela <abreupamm@gmail.com>
2022-04-18 21:29:08 -0300

    questao 5 pronta

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  let qtdAnimal;
+  qtdAnimal = data.species.reduce((qtds, species) => {
+    qtds[species.name] = species.residents.length;
+    return qtds;
+  }, {});
+  if (animal !== undefined) {
+    const animais = data.species.filter((specie) => specie.name === animal.specie);
+    qtdAnimal = animais[0].residents.reduce((qtd, animal1) => {
+      if (Object.keys(animal).length === 1 || animal1.sex === animal.sex) {
+        qtd += 1;
+      }
+      return qtd;
+    }, 0);
+  }
+  return qtdAnimal;
 }
 
 module.exports = countAnimals;
