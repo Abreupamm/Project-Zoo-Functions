1d2ae3f46a2c7c25ca43e05bbea5113cad4aa679
pamela <abreupamm@gmail.com>
2022-04-19 18:48:50 -0300

    questao 6 pronta

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const age = entrants.reduce((acc, element) => {
+    let resultado;
+    if (element.age < 18) {
+      resultado = { child: acc.child + 1, adult: acc.adult, senior: acc.senior };
+    }
+    if (element.age < 50 && element.age >= 18) {
+      resultado = { child: acc.child, adult: acc.adult + 1, senior: acc.senior };
+    }
+    if (element.age >= 50) {
+      resultado = { child: acc.child, adult: acc.adult, senior: acc.senior + 1 };
+    }
+    return resultado;
+  }, { child: 0, adult: 0, senior: 0 });
+  return age;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const idade = countEntrants(entrants);
+  const valueChild = idade.child * data.prices.child;
+  const valueAdult = idade.adult * data.prices.adult;
+  const valueSenior = idade.senior * data.prices.senior;
+  return valueChild + valueAdult + valueSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
